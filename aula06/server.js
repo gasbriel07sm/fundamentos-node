3d6e4c71b64c19607eea4fb05e27203f1eb17f2c
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-26 14:08:44 -0300

    Express - introduction

---
+++ aula06/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express(); // criando uma instância do Express
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor rodando na porta 3000');
+});
+
+//         Criar,  Ler,  Atualizar, Deletar
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//         POST,   GET,  PUT,    DELETE
+
+// http://meusite.com/ <- GET -> entregue a página /
+// http://meusite.com/sobre <- GET -> entregue a página sobre
+// http://meusite.com/contato <- GET -> entregue a página contato
+
+app.get('/', (req, res) => {
+  res.send(`
+    <form action="/" method="POST">
+    Nome: <input type="text" name="nome" />
+    <button>Enviar</button>
+    </form>
+  `);
+});
+
+app.post('/', (req, res) => {
+  res.send('Formulário recebido');
+});
+
+app.get('/contato', (req, res) => {
+  res.send('Página de contato');
+});
