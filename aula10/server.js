2c4e5e5fb095370e635b9550cb4793dffa66ff7f
gabrielmiranda07sm <gasmiranda07@gmail.com>
2026-05-26 15:07:24 -0300

    Express - Views

---
+++ aula10/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express(); // criando uma instância do Express
+const routes = require('./routes'); // importando as rotas
+const path = require('path'); // módulo para lidar com caminhos de arquivos
+
+app.use(express.urlencoded({ extended: true })); // para interpretar dados de formulários
+app.set('views', path.resolve(__dirname, 'src', 'views')); // definindo o diretório das views
+app.set('view engine', 'ejs'); // definindo o motor de template como EJS
+app.use(routes); // usando as rotas
+
+app.listen(3000, () => {
+  console.log('Acessar http://localhost:3000');
+  console.log('Servidor rodando na porta 3000');
+});
